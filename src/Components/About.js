19a6d00f93f374b6235e5a7057392ad25cd41a37
jokale <kalemajoanna@gmail.com>
2020-12-18 19:13:10 +0000

    add about details

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,8 +4,11 @@ class About extends React.Component {
     render(){
         return(
             <div>
+                <center>
                 <h1>My Diary</h1>
-            </div>
+                <h4>My diary is a place for you to express yourself without the stress of pain and paper</h4>
+                </center> 
+           </div>
         )
     }
 }
