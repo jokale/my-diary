f20776a447414e2ef76134669349acbb797e4ee4
jokale <kalemajoanna@gmail.com>
2020-12-18 18:22:42 +0000

    add styling to forms

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -5,6 +5,17 @@ class SignUp extends React.Component {
         return(
             <div>
                 <h1>My Diary Sign up form</h1>
+                <form>
+                    <label>Name</label> <br></br>
+                    <input placeholder="Name" type="text"></input> <br></br>
+                    <label>Email</label> <br></br>
+                    <input placeholder="Email" type="text"></input> <br></br>
+                    <label>Password</label> <br></br>
+                    <input type="password" placeholder="password" ></input> <br></br>
+                    <label>Enter password once more</label> <br></br>
+                    <input type="password" placeholder="password"></input> <br></br>
+                    <br></br> <button type="submit">Create your diary</button>
+                </form>
             </div>
         )
     }
